e459ed4e582fd4f84cd1d33b841ae684577ec647
Asshu-06 <adduriaswani@gmail.com>
2026-08-11 16:46:43 +0530

    Fix CORS

--- backend/app.py
+++ backend/app.py
@@ -11,12 +11,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://careerpilot-ai-seven-eta.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 def home():
